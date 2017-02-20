2596772da22ceaadec1b49b6d539c7f077027879
Alana Luna <Alana Luna>
2017-02-20 00:06:57 -0300

    Variables: Introduction

---
+++ aula10/src/com/training/javacourse/aula10/Variables.java
@@ -0,0 +1,16 @@
+package com.training.javacourse.aula10;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int age = 19;
+		String myName = "Alana";
+		String myDog = "Amber";
+		
+		System.out.println("Informations: " + age + ", " + myName + ", " + myDog);
+		
+
+	}
+
+}
