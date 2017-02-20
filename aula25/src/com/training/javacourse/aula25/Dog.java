32c7f514677a3acf8d3677e5818fdd0a6f5e2b66
Alana Luna <Alana Luna>
2017-02-20 00:35:13 -0300

    Classes and methods

---
+++ aula25/src/com/training/javacourse/aula25/Dog.java
@@ -0,0 +1,23 @@
+package com.training.javacourse.aula25;
+
+public class Dog {
+
+	String name;
+	String color;
+	String sex;
+	int age;
+	
+	void bark(){
+		System.out.println(name + " is barking");
+	}
+	
+	void sleep(){
+		System.out.println(name + " is sleeping");
+	}
+	
+	void play(){
+		System.out.println(name + " is playing");
+	}
+	
+	
+}
---
+++ aula25/src/com/training/javacourse/aula25/DogTest.java
@@ -0,0 +1,18 @@
+package com.training.javacourse.aula25;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		
+		Dog husky = new Dog();
+		
+		husky.name = "Amber";
+		husky.age = 4;
+		husky.color = "brown";
+		husky.sex = "female";
+		
+		husky.sleep();
+
+	}
+
+}
