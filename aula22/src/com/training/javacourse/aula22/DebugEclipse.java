1d7070f5b8581e6a217992457a35959562f13a58
Alana Luna <Alana Luna>
2017-02-20 00:15:38 -0300

    Debug - Eclipse

---
+++ aula22/src/com/training/javacourse/aula22/DebugEclipse.java
@@ -0,0 +1,19 @@
+package com.training.javacourse.aula22;
+
+public class DebugEclipse {
+
+	public static void main(String[] args) {
+		
+		int grades[] = new int[3];
+		
+		//put breakpoint below
+		for(int i = 0; i < 4; i++){
+			
+			grades[i] = i;
+			
+			
+		}
+
+	}
+
+}
